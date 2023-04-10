7e81d2d79f26976c3b97af2a011e5b1cdfa47963
RaphMar2022 <raphmar2022@hotmail.com>
2023-04-10 22:02:32 +0200

    0.1.0

---
+++ TestApp/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TestApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.ForeColor = Color.FromArgb(255, 255, 255);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 450);
+            panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Bottom;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(24, 24, 24);
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(12, 12, 12);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(0, 410);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Settings";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(24, 24, 24);
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(12, 12, 12);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Home";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(32, 32, 32);
+            panel2.Dock = DockStyle.Fill;
+            panel2.ForeColor = Color.FromArgb(255, 255, 255);
+            panel2.Location = new Point(200, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(600, 450);
+            panel2.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            ForeColor = Color.FromArgb(255, 255, 255);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ TestApp/MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.UI.Composition.BackdropBrushes;
+using Windows.UI.Controls;
+using Windows.UI.Theming;
+
+namespace TestApp
+{
+    public partial class MainForm : Window
+    {
+        MicaBrush _micaBrush;
+        public MainForm()
+        {
+            InitializeComponent();
+            BackdropApplied = true;
+            InitializeComponent();
+            _micaBrush = new MicaBrush();
+            _micaBrush.TargetWindow = this;
+            this.ThemeChanged += MainForm_ThemeChanged;
+        }
+
+        private void MainForm_ThemeChanged(object? sender, EventArgs e)
+        {
+            if (panel2.Controls.Count >= 1)
+                ((Windows.UI.Controls.Window)panel2.Controls[0]).ElementTheme = this.ElementTheme;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _micaBrush.ApplyBackdrop();
+            ElementTheme = SystemThemeHandler.AppTheme;
+            SystemThemeHandler.OnThemeChanged += SystemThemeHandler_OnThemeChanged;
+        }
+
+        void Navigate(Window f)
+        {
+            foreach (System.Windows.Forms.Control c in panel2.Controls)
+            {
+                Controls.Remove(c);
+            }
+            f.TopMost = false;
+            f.TopLevel = false;
+            f.ElementTheme = this.ElementTheme;
+            f.Dock = DockStyle.Fill;
+            f.FormBorderStyle = FormBorderStyle.None;
+            panel2.Controls.Add(f);
+            f.Show();
+        }
+
+        private void SystemThemeHandler_OnThemeChanged(object? sender, EventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                ElementTheme = SystemThemeHandler.AppTheme;
+                ((Windows.UI.Controls.Window)panel2.Controls[0]).ElementTheme = SystemThemeHandler.AppTheme;
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navigate(new Form1());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Navigate(new Form2());
+        }
+    }
+}
